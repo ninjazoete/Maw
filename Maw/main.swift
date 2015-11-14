cb16ac83fdf7aeda6c2acf71c839cf89ac25f20b
Andrzej Spiess <andrzej.spiess@gmail.com>
2015-11-14 09:33:43 +0100

    Initial Commit

---
+++ Maw/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Maw
+//
+//  Created by Andrzej Spiess on 14/11/15.
+//  Copyright © 2015 Andrzej Spiess. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
